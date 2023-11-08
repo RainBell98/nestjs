6f88d750721ab17c9512b826123e645bc013060d
RainBell98 <ttiger45@naver.com>
2023-11-08 20:32:58 +0900

    Study: nestjs(jwt,)

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrKey: 'Secret1234',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne({
+      where: { username: username },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,34 @@
 import { DataSource, EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async creatUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = this.create({
+      username: username,
+      password: hashPassword,
+    });
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
